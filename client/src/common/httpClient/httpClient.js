16a1dc0f25333c7e76c2a2bdb94f55039155815c
byohannes <byohannes44@gmail.com>
2020-09-04 01:31:28 +0100

    prepare to deploy

--- client/src/common/httpClient/httpClient.js
+++ client/src/common/httpClient/httpClient.js
@@ -1,7 +1,6 @@
-import axios from "axios";
-const { REACT_APP_BASE_URL } = process.env;
+import axios from "axios"; 
 const httpClient = axios.create({
-  baseURL: REACT_APP_BASE_URL || "http://localhost:5000",
+  baseURL: "http://localhost:5000",
 });
 
 export default httpClient;
