4d5823399f81828b0d4049838cb85c92c30a4693
byohannes <byohannes44@gmail.com>
2020-09-05 17:44:13 +0100

    removed httpclient link

--- client/src/common/httpClient/httpClient.js
+++ client/src/common/httpClient/httpClient.js
@@ -1,6 +1,6 @@
-import axios from "axios"; 
-const httpClient = axios.create({
-  baseURL: "http://localhost:3490",
-});
+// import axios from "axios"; 
+// const httpClient = axios.create({
+//   baseURL: "http://localhost:3490",
+// });
 
-export default httpClient;
+// export default httpClient;
